a3c595e389948b8426c1c6f5fb86d84c45a826f3
AliasTheBlank <aliastheblank@gmail.com>
2023-11-07 21:38:16 -0500

    Initial Commit

---
+++ Nebulingo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nebulingo
+//
+//  Created by Francisco Vargas on 11/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
