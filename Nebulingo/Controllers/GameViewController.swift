d92db230d0e4c59f7b4aaf04b5562ea7f0c24793
jose <joshrs23@gmail.com>
2023-12-01 11:05:43 -0500

    validations for the verb

--- Nebulingo/Controllers/GameViewController.swift
+++ Nebulingo/Controllers/GameViewController.swift
@@ -18,6 +18,14 @@ class GameViewController: UIViewController {
     @IBAction func btnBackTouchUpInside(_ sender: Any) {
         self.dismiss(animated: true)
     }
+    
+    @IBOutlet weak var tvJe: UITextField!
+    @IBOutlet weak var tvTu: UITextField!
+    @IBOutlet weak var tvIl: UITextField!
+    @IBOutlet weak var tvNous: UITextField!
+    @IBOutlet weak var tvVous: UITextField!
+    @IBOutlet weak var tvIls: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         txtVerb.text = receivedVerb?.infinitif
@@ -34,6 +42,68 @@ class GameViewController: UIViewController {
             break;
         default:
             break;
+        }
+    }
+    
+    @IBAction func validateAns(_ sender: Any) {
+        
+        if tvJe.text?.isEmpty ?? true ||
+               tvTu.text?.isEmpty ?? true ||
+               tvIl.text?.isEmpty ?? true ||
+               tvNous.text?.isEmpty ?? true ||
+               tvVous.text?.isEmpty ?? true ||
+            tvIls.text?.isEmpty ?? true {
+            Toast.ok(view: self, title: "Error", message: "Please fill all the text fields.")
+        }else{
+            switch level{
+            case "Beginner":
+                if(tvJe.text! == receivedVerb?.present?[1]){
+                    tvJe.backgroundColor = UIColor.green
+                }else{
+                    tvJe.backgroundColor = UIColor.red
+                }
+                
+                if(tvTu.text! == receivedVerb?.present?[2]){
+                    tvTu.backgroundColor = UIColor.green
+                }else{
+                    tvTu.backgroundColor = UIColor.red
+                }
+                
+                if(tvIl.text! == receivedVerb?.present?[3]){
+                    tvIl.backgroundColor = UIColor.green
+                }else{
+                    tvIl.backgroundColor = UIColor.red
+                }
+                
+                if(tvNous.text! == receivedVerb?.present?[4]){
+                    tvNous.backgroundColor = UIColor.green
+                }else{
+                    tvNous.backgroundColor = UIColor.red
+                }
+                
+                if(tvVous.text! == receivedVerb?.present?[5]){
+                    tvVous.backgroundColor = UIColor.green
+                }else{
+                    tvVous.backgroundColor = UIColor.red
+                }
+                
+                if(tvIls.text! == receivedVerb?.present?[6]){
+                    tvIls.backgroundColor = UIColor.green
+                }else{
+                    tvIls.backgroundColor = UIColor.red
+                }
+                
+                
+                break;
+            case "Intermidiate":
+                txtTime.text = "Future"
+                break;
+            case "Advanced":
+                txtTime.text = "Past"
+                break;
+            default:
+                break;
+            }
         }
     }
 }
